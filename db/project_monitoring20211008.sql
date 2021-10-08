db928c32be587a46f1ab1e9e98eab8493d62de7a
parksol <39298191+parkeunsol@users.noreply.github.com>
2021-10-08 14:42:04 +0900

    db

---
+++ db/project_monitoring20211008.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: project
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `monitoring20211008`
+--
+
+DROP TABLE IF EXISTS `monitoring20211008`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `monitoring20211008` (
+  `monitor_id` int NOT NULL AUTO_INCREMENT,
+  `mold_temp` double NOT NULL,
+  `melt_temp` double NOT NULL,
+  `injection_speed` double NOT NULL,
+  `hold_pressure` double NOT NULL,
+  `injection_time` double NOT NULL,
+  `hold_time` double NOT NULL,
+  `filling_time` double NOT NULL,
+  `cycle_time` double NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `z` double NOT NULL,
+  `stud_h` double NOT NULL,
+  `stud_d` double NOT NULL,
+  `thick` double NOT NULL,
+  `defect` varchar(5) NOT NULL,
+  `date` text NOT NULL,
+  PRIMARY KEY (`monitor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `monitoring20211008`
+--
+
+LOCK TABLES `monitoring20211008` WRITE;
+/*!40000 ALTER TABLE `monitoring20211008` DISABLE KEYS */;
+/*!40000 ALTER TABLE `monitoring20211008` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-08 14:40:36
